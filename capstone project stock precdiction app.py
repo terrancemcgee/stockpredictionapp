bf3bc5eea18ba147421a5e99b86f08d79a76c153
TerranceMcGee <49994878+terrancemcgee@users.noreply.github.com>
2022-03-29 22:08:38 -0500

    Update capstone project stock precdiction app.py

--- capstone project stock precdiction app.py
+++ capstone project stock precdiction app.py
@@ -1,49 +1,64 @@
-from pyspark.sql import SQLContext
-import base64
-import pandas as pd
-from google.cloud import bigquery
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow import keras
-from tensorflow.python.lib.io import file_io
-import pymysql
-def iex_predict(event, context):
- “””Triggered from a message on a Cloud Pub/Sub topic.
- Args:
- event (dict): Event payload.
- context (google.cloud.functions.Context): Metadata for the event.
- “””
- pubsub_message = base64.b64decode(event[‘data’]).decode(‘utf-8’)
- stock_symbol = “SPY”
- 
- query = ‘select * from iex_history.’ + stock_symbol
- client = bigquery.Client()
- 
- df = (
- client.query(query)
- .result()
- .to_dataframe()
- )
- 
- scaler = MinMaxScaler()
- tmp = scaler.fit_transform(df)
- 
- model_file = file_io.FileIO(‘gs://yourbucket/model.h5’, mode=’rb’)
- temp_model_location = ‘./temp_model.h5’
- temp_model_file = open(temp_model_location, ‘wb’)
- temp_model_file.write(model_file.read())
- temp_model_file.close()
- model_file.close()
- model = keras.models.load_model(temp_model_location)
- 
- result = model.predict(tmp)
- scaler.fit(df.avg.values.reshape(-1,1))
- result = scaler.inverse_transform(result.reshape(-1,1))
- result = [stock_symbol, df.tail(1).avg.values[0], result]
- 
- cnx = pymysql.connect(user=’usr’, password=’pwd’,
- host=’host’, db=’iex_predict’)
- 
- with cnx.cursor() as cursor:
-   cursor.executemany(‘INSERT INTO iex_predict values (%s, %s, %s);’, results)
-   cnx.commit()
- cnx.close()
+import streamlit as st
+from datetime import date
+
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title('Stock Forecast App')
+
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
+
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
+
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+	
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
+
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
